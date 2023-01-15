c804585944ecc190ec55137545cae40ca1bee04e
YuliiaZaiets <113512442+YuliiaZaiets@users.noreply.github.com>
2023-01-15 17:58:46 +0100

    addes the search form

--- src/index.js
+++ src/index.js
@@ -1,38 +1,64 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round (response.data.main.temp);
-cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round (response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+   iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
 let apiKey = "97f8e93f00107773f88eafd933ce86b7";
 let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 
-function search(event) {
+function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
+  search(searchInput.value);
 }
-let form = document.querySelector(".search-form");
-form.addEventListener("submit", search);
 
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let currentDate = document.querySelector("#current-date");
-currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", handleSubmit);
+
 
 function followFahrenheit() {
   let currentFahrenheitTemp = document.querySelector(".temperature");
